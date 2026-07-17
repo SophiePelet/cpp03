c3be0b30efe62ad112f19f18d3af11f1f37d2669
Sophie <peletsophie@gmail.com>
2026-07-17 20:20:21 +0200

    feat: add more tests to the main of ex00 and ex01

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -6,26 +6,42 @@
 /*   By: sopelet <sopelet@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/07/02 11:17:55 by sopelet           #+#    #+#             */
-/*   Updated: 2026/07/02 16:03:29 by sopelet          ###   ########.fr       */
+/*   Updated: 2026/07/17 19:40:33 by sopelet          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ClapTrap.hpp"
 
 int	main() {
+	std::cout << BLUE << "/* --------- Testing constructors, copy constructor and copy assignment --------- */\n"
+		<< RESET;
 	ClapTrap	clap1("Clap");
 	ClapTrap	clap2("Kewan");
 	ClapTrap	clap3 = clap2;
+	ClapTrap	clap4("test1");
+	ClapTrap	clap5 ("test2");
 
+	clap4 = clap5;
+
+	std::cout << BLUE << "\n/* --------------- Testing attack()/beRepaired and Energy exhaustion ----------- */\n"
+        << RESET;
 	for (size_t i = 0; i < 11; ++i)
 		clap1.attack("Kewan");
-	
+		
 	clap2.takeDamage(9);
-	clap2.attack("Clap");
-	clap2.attack("Clap");
-	
 	for (size_t i = 0; i < 9; ++i)
 		clap2.beRepaired(1);
+	
+	std::cout << BLUE << "\n/* ------------------- Testing takeDamage() and Death ---------------- */\n"
+        << RESET;
+	clap2.takeDamage(5);
+    clap2.beRepaired(2);
+    clap2.takeDamage(20);
+    clap2.attack("Clap");
+    clap2.beRepaired(5);
+
+	std::cout << BLUE << "\n/* ------------------- Object destruction ---------------- */\n"
+        << RESET;
 	
 	return (0);
 }
--- ex01/main.cpp
+++ ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: sopelet <sopelet@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/07/03 14:51:06 by sophie            #+#    #+#             */
-/*   Updated: 2026/07/04 11:58:03 by sopelet          ###   ########.fr       */
+/*   Updated: 2026/07/17 19:40:43 by sopelet          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,20 +14,31 @@
 #include "ScavTrap.hpp"
 
 int	main() {
+	std::cout << BLUE << "/* --------- Testing constructors, copy constructor and copy assignment --------- */\n"
+		<< RESET;
 	ScavTrap	scav1("Scav");
 	ScavTrap	scav2("Kewan");
 	ScavTrap	scav3 = scav2;
+	ScavTrap	scav4("test1");
+	ScavTrap	scav5("test2");
 
-	for (size_t i = 0; i < 11; ++i)
-		scav1.attack("Kewan");
-	
-	scav2.takeDamage(9);
-	scav2.attack("Scav");
-	scav2.attack("Scav");
-	scav2.guardGate();
-	
-	for (size_t i = 0; i < 9; ++i)
-		scav2.beRepaired(1);
-	
+	scav5 = scav4;
+
+	std::cout << BLUE << "\n/* --------- Testing attack() and Energy exhaustion (50 EP) --------- */\n" << RESET;
+    for (size_t i = 0; i < 51; ++i)
+        scav1.attack("Kewan");
+    
+    std::cout << BLUE << "\n/* --------- Testing guardGate() --------- */\n" << RESET;
+    scav2.guardGate();
+    
+    std::cout << BLUE << "\n/* --------- Testing Damage, Healing, and Death (100 HP) --------- */\n" << RESET;
+    scav2.takeDamage(50);
+    scav2.takeDamage(100); 
+    scav2.attack("Scav");
+    scav2.guardGate();
+
+	std::cout << BLUE << "\n/* ------------------- Object destruction ---------------- */\n"
+        << RESET;
+
 	return (0);
 }
