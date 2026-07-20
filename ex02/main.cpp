c504e8254923dda2ac9bc588b5d7419af8286cc7
Sophie <peletsophie@gmail.com>
2026-07-20 14:41:35 +0200

    feat: add tests to ex02 and ex03 `main`

--- ex02/main.cpp
+++ ex02/main.cpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: sophie <sophie@student.42.fr>              +#+  +:+       +#+        */
+/*   By: sopelet <sopelet@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/07/03 14:51:06 by sophie            #+#    #+#             */
-/*   Updated: 2026/07/03 15:22:02 by sophie           ###   ########.fr       */
+/*   Updated: 2026/07/20 11:34:22 by sopelet          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,20 +14,34 @@
 #include "FragTrap.hpp"
 
 int	main() {
+	std::cout << BLUE << "/* --------- Testing constructors, copy constructor and copy assignment --------- */\n"
+		<< RESET;
 	FragTrap	frag1("Frag");
 	FragTrap	frag2("Kewan");
 	FragTrap	frag3 = frag2;
+	FragTrap	frag4("test1");
+	FragTrap	frag5("test2");
 
-	for (size_t i = 0; i < 11; ++i)
+	frag5 = frag4;
+
+	std::cout << BLUE << "\n/* ------------ Testing attack() and Energy exhaustion ------------- */\n" << RESET;
+	for (size_t i = 0; i < 101; ++i)
 		frag1.attack("Kewan");
 	
-	frag2.takeDamage(9);
-	frag2.attack("Frag");
-	frag2.attack("Frag");
+	std::cout << BLUE << "\n/* -------------- Testing highFiveGuys() -------------- */\n" << RESET;
 	frag2.highFivesGuys();
-	
-	for (size_t i = 0; i < 9; ++i)
-		frag2.beRepaired(1);
+
+	std::cout << BLUE << "\n/* --------- Testing Damage, Healing, and Death (100 HP) --------- */\n" << RESET;
+	frag2.takeDamage(50);
+    frag2.beRepaired(20);
+
+    frag2.takeDamage(100); 
+
+    frag2.attack("Frag");
+    frag2.highFivesGuys();
+
+	std::cout << BLUE << "\n/* ------------------- Object destruction ---------------- */\n"
+        << RESET;
 	
 	return (0);
 }
--- ex03/main.cpp
+++ ex03/main.cpp
@@ -6,29 +6,49 @@
 /*   By: sopelet <sopelet@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/07/03 14:51:06 by sophie            #+#    #+#             */
-/*   Updated: 2026/07/04 12:24:50 by sopelet          ###   ########.fr       */
+/*   Updated: 2026/07/20 13:16:47 by sopelet          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "DiamondTrap.hpp"
 
 int	main() {
-	DiamondTrap	Diamond0;
-	DiamondTrap	Diamond1("Diamond");
-	DiamondTrap	Diamond2("Kewan");
-	DiamondTrap	Diamond3 = Diamond2;
+	std::cout << BLUE << "/* --------- Testing constructors, copy constructor and copy assignment --------- */\n"
+		<< RESET;
+	DiamondTrap	diamond0;
+	DiamondTrap	diamond1("Diamond");
+	DiamondTrap	diamond2("Kewan");
+	DiamondTrap	diamond3 = diamond2;
+	DiamondTrap	diamond4("test1");
+	DiamondTrap	diamond5("test2");
 
-	Diamond2.whoAmI();
-	for (size_t i = 0; i < 11; ++i)
-		Diamond1.attack("Kewan");
-	Diamond2.takeDamage(9);
-	Diamond2.attack("Diamond");
-	Diamond2.attack("Diamond");
-	Diamond1.guardGate();
-	Diamond3.highFivesGuys();
+	diamond5 = diamond4;
+
+	std::cout << BLUE << "/* ----------------- Testing whoAmI() ----------------- */\n" << RESET;
+	diamond0.whoAmI();
+	diamond2.whoAmI();
+	diamond5.whoAmI();
+
+	std::cout << BLUE << "\n/* ------------ Testing attack() and Energy exhaustion ------------- */\n" << RESET;
+	for (size_t i = 0; i < 51; ++i)
+		diamond1.attack("Kewan");
+
+	std::cout << BLUE << "\n/* ------------ Testing Inherited Special Abilities ------------- */\n" << RESET;
+	diamond1.guardGate();
+	diamond3.highFivesGuys();
 	
-	for (size_t i = 0; i < 9; ++i)
-		Diamond2.beRepaired(1);
-	
+	std::cout << BLUE << "\n/* ------------ Testing Damage, Healing, and Death (100 HP) ------------- */\n" << RESET;
+
+	diamond2.takeDamage(50);
+	diamond2.beRepaired(20);
+
+	diamond2.takeDamage(100);
+
+	diamond2.attack("Diamond");
+	diamond2.guardGate();
+	diamond2.whoAmI();
+
+	std::cout << BLUE << "\n/* ------------------- Object destruction ---------------- */\n"
+        << RESET;
 	return (0);
 }
